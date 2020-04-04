441023e54734466c0763fe9bde0f9e1ede6a31ee
Jackson Mooring <me@jacksonmooring.com>
2020-04-04 14:34:27 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+# Home dir
+cd
+
+# Install zsh
+pamac install zsh curl git
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Start Installing NodeJS
+echo "\n\n\n\n==== Installing NodeJS =====\n\n\n\n"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
+
+# This loads NVM
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# This installs Node
+nvm install node
+
+
+# Install Apps
+sudo pamac install brave
+sudo snap install discord
+
+# Theme
+git clone https://github.com/vinceliuice/McMojave-kde theme
+cd theme && ./install.sh
